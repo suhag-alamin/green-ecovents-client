f7d8d1e03ee9352929a4f3155f03d01aa56bc972
suhag-alamin <developersuhag@gmail.com>
2023-10-20 00:26:23 +0600

    fixed api route error

--- src/app/api/file/route.ts
+++ src/app/api/file/route.ts
@@ -1,7 +1,6 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(req: NextRequest, res: NextResponse) {
   return NextResponse.json({
     name: "File uploaded",
   });
